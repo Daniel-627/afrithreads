0d742592e417bd3d9f19bfed430898930469d4ab
Daniel-627 <ochiengdaniel627@gmail.com>
2025-07-03 00:09:13 +0300

    Created Navbar structure

---
+++ components/Navbar/CartIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function CartIcon({}: Props) {
+  return (
+    <div>CartIcon</div>
+  )
+}
---
+++ components/Navbar/NavLinks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function NavLinks({}: Props) {
+  return (
+    <div>NavLinks</div>
+  )
+}
