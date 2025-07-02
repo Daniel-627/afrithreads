8b247be533e83f550d2a5a3082a62a600f85e29e
Daniel-627 <ochiengdaniel627@gmail.com>
2025-07-03 00:25:19 +0300

    Created banners structure

---
+++ components/Banners/HeroSwiperBanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function HeroSwiperBanner({}: Props) {
+  return (
+    <div>HeroSwiperBanner</div>
+  )
+}
---
+++ components/Banners/MiniBannerOne.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function MiniBannerOne({}: Props) {
+  return (
+    <div>MiniBannerOne</div>
+  )
+}
