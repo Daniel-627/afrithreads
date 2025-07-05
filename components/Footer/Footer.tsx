fed69725b28aa3dcc82a9b2fe2c1360942ee6eda
Daniel-627 <ochiengdaniel627@gmail.com>
2025-07-06 00:43:14 +0300

    changing footer bg

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import NewsLetter from './NewsLetter'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white pt-10 pb-4 px-4">
+    <footer className=" pt-10 pb-4 px-4">
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         <Brand />
         <Info />
